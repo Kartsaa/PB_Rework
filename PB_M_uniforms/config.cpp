a77507b81a3ed57fcd9ca6a4e950fd08f05bb8fd
Kartsaa <60628805+Kartsaa@users.noreply.github.com>
2024-02-27 10:36:24 +0200

    Uniforms and Main

---
+++ PB_M_uniforms/config.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.10
+//https://mikero.bytex.digital/Downloads
+//'now' is Mon Dec 11 19:06:02 2023 : 'file' last modified on Mon Nov 09 00:31:48 2020
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class PB_M_Uniforms
+	{
+		version = "1.1.3";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = "1.1.3";
+		requiredAddons[] = {"PB_Main"};
+	};
+};
