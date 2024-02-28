7ce8ea9177a2c2e51ea0188080925b7eddb339fe
Kartsaa <60628805+Kartsaa@users.noreply.github.com>
2024-02-28 13:19:30 +0200

    Work
    - MTP Stuff
    - Abandoded IOTV Vests

--- PB_British_Kit/config.cpp
+++ PB_British_Kit/config.cpp
@@ -47,75 +47,179 @@ class CfgVehicles
 	class B_AssaultPack_Base;
 	class B_Kitbag_Base;
 ///UNIS///
-	class PB_DDPM_ACU_B: B_Soldier_base_F
+	class PB_DDPM_Gora_B: B_Soldier_base_F
 	{
 		scope = 1;
 		author = "Kartsa";
-        model = "\PB_M_uniforms\A_CU\Aegis_OCombatUniform_F.p3d";
-		picture = "\PB_Main\UI\uniform_test.paa";
-        hiddenSelections[] = {"camo","camo1","camo2"};
-        hiddenSelectionsTextures[] = 
-        {
-            "PB_British_Kit\data\ddpm_acu_camo_co.paa",
-            "PB_British_Kit\data\ddpm_acu_camo1_co.paa",
-            "PB_British_Kit\data\TacGloves_khk_CO.paa"
-        };
-	};
-	class PB_DDPM_ACU_B_R: B_Soldier_base_F
-	{
-		scope = 1;
-		author = "Kartsa";
-        model = "\PB_M_uniforms\A_CU\Aegis_OCombatUniform_02_F.p3d";
-		picture = "\PB_Main\UI\uniform_test.paa";
-        hiddenSelections[] = {"camo","camo1","camo2"};
-        hiddenSelectionsTextures[] = 
-        {
-            "PB_British_Kit\data\ddpm_acu_camo_co.paa",
-            "PB_British_Kit\data\ddpm_acu_camo1_co.paa",
-            "PB_British_Kit\data\TacGloves_khk_CO.paa"
-        };
-	};
-	class PB_MTP_CTRG_U_B: B_Soldier_base_F
-	{
-		scope = 1;
-		author = "Kartsa";
-		model = "\A3\Characters_F_Exp\BLUFOR\B_CTRG_Soldier_01_F.p3d";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_01.p3d";
 		picture = "\PB_Main\UI\uniform_test.paa";
 		hiddenSelections[] = {"camo","insignia"};
-		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\MTP_CTRG_U_01_CO.paa"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DDPM_gora_co.paa"};
 	};
-	class PB_MTP_CTRG_U_B_R: B_Soldier_base_F
+	class PB_DDPM_Gora_B_RU: B_Soldier_base_F
 	{
 		scope = 1;
 		author = "Kartsa";
-		model = "\A3\Characters_F_Exp\BLUFOR\B_CTRG_Soldier_02_F.p3d";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_02.p3d";
 		picture = "\PB_Main\UI\uniform_test.paa";
 		hiddenSelections[] = {"camo","insignia"};
-		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\MTP_CTRG_U_01_CO.paa"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DDPM_gora_co.paa"};
+	};
+	class PB_DDPM_GoraG_B: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_03.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","insignia"};
+		
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DDPM_gora_co.paa","PB_British_Kit\data\TacGloves_khk_CO.paa"};
+	};
+	class PB_DDPM_GoraG_B_RU: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_04.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DDPM_gora_co.paa","PB_British_Kit\data\TacGloves_khk_CO.paa"};
+	};
+	class PB_DPM_Gora_B: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_01.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DPM_gora_co.paa"};
+	};
+	class PB_DPM_Gora_B_RU: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_02.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DPM_gora_co.paa"};
+	};
+	class PB_DPM_GoraG_B: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_03.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DPM_gora_co.paa","PB_British_Kit\data\TacGloves_CLK_CO.paa"};
+	};
+	class PB_DPM_GoraG_B_RU: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_04.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\DPM_gora_co.paa","PB_British_Kit\data\TacGloves_CLK_CO.paa"};
 	};
 	class PB_MTP_U_B: B_Soldier_base_F
 	{
 		scope = 1;
-		author = "Jakx2210 (Caboose), Kartsa";
-		model = "\A3\characters_f_beta\INDEP\ia_soldier_01.p3d";
+		author = "Kartsa";
+		model = "\A3\characters_F\BLUFOR\b_soldier_01.p3d";
 		picture = "\PB_Main\UI\uniform_test.paa";
 		hiddenSelections[] = {"camo","insignia"};
-		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\MTP_Uniform.paa"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\MTP_U_01_CO.paa"};
 	};
 	class PB_MTP_U_B_R: B_Soldier_base_F
 	{
 		scope = 1;
-		author = "Jakx2210 (Caboose), Kartsa";
-		model = "\A3\characters_f_beta\INDEP\ia_soldier_02.p3d";
+		author = "Kartsa";
+		model = "\A3\characters_F\BLUFOR\b_soldier_03.p3d";
 		picture = "\PB_Main\UI\uniform_test.paa";
 		hiddenSelections[] = {"camo","insignia"};
-		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\MTP_Uniform.paa"};
+		hiddenSelectionsTextures[] = {"\PB_British_Kit\data\MTP_U_01_CO.paa"};
+	};
+	class PB_MTP_ACU_B: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\A_CU\Aegis_OCombatUniform_F.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","camo2"};
+        hiddenSelectionsTextures[] = 
+        {
+            "PB_British_Kit\data\MTP_acu_camo_co.paa",
+            "PB_British_Kit\data\MTP_acu_camo1_co.paa",
+            "PB_British_Kit\data\TacGloves_khk_CO.paa"
+        };
+	};
+	class PB_MTP_ACU_B_R: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\A_CU\Aegis_OCombatUniform_02_F.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","camo2"};
+        hiddenSelectionsTextures[] = 
+        {
+            "PB_British_Kit\data\MTP_acu_camo_co.paa",
+            "PB_British_Kit\data\MTP_acu_camo1_co.paa",
+            "PB_British_Kit\data\TacGloves_khk_CO.paa"
+        };
+	};
+	///BACKPACKS////
+	class PB_CARRYALL_MTP: B_Kitbag_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		displayName = "[MTP] Carryall Backpack";
+		picture = "PB_Main\ui\kitbag_test.paa";
+		model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Tortila";
+		hiddenSelectionsTextures[] = {"PB_British_Kit\data\MTP_CB_CO.paa"};	
+		maximumLoad = 320;
+		mass = 70;
+	};
+	class PB_Kitbag_MTP: B_Kitbag_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		displayName = "[MTP] Kitbag";
+		picture = "PB_Main\ui\kitbag_test.paa";
+		model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast";
+		hiddenSelectionsTextures[] = {"PB_British_Kit\data\MTP_KB_CO.paa"};	
+		maximumLoad = 200;
+		mass = 40;
+	};
+	class PB_CARRYALL_DPM: B_Kitbag_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		displayName = "[DPM] Carryall Backpack";
+		picture = "PB_Main\ui\kitbag_test.paa";
+		model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Tortila";
+		hiddenSelectionsTextures[] = {"PB_British_Kit\data\DPM_CB_CO.paa"};	
+		maximumLoad = 320;
+		mass = 70;
+	};
+	class PB_Kitbag_DPM: B_Kitbag_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		displayName = "[DPM] Kitbag";
+		picture = "PB_Main\ui\kitbag_test.paa";
+		model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Fast";
+		hiddenSelectionsTextures[] = {"PB_British_Kit\data\DPM_KB_CO.paa"};	
+		maximumLoad = 200;
+		mass = 40;
 	};
 };
 class cfgWeapons
 {
 /////INHERITANCE////
 	class ContainerSupply;
+	class Supply120: ContainerSupply
+	{
+		maximumLoad = 120;
+	};
 	class Supply30: ContainerSupply
 	{
 		maximumLoad = 30;
@@ -134,68 +238,136 @@ class cfgWeapons
 		class ItemInfo;
 	};
 ////UNIS////
-	class PB_DDPM_ACU_U: Uniform_Base
+	class PB_DDPM_Gora: Uniform_Base
 	{
 		scope = 2;
 		author = "Kartsa";
-		displayName = "[DDPM] Combat Uniform";
+		displayName = "[DDPM] Fatigues";
 		picture = "\PB_Main\ui\uniform_test.paa";
 		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
 		class ItemInfo: UniformItem
 		{
 			uniformModel = "-";
-			uniformClass = "PB_DDPM_ACU_B";
+			uniformClass = "PB_DDPM_Gora_B";
 			containerClass = "Supply45";
 			mass = 50;
 			allowedSlots[] = {"701","801","901"};
 			armor = 0;
 		};
 	};
-	class PB_DDPM_ACU_U_R: Uniform_Base
+	class PB_DDPM_Gora_RU: Uniform_Base
 	{
 		scope = 2;
 		author = "Kartsa";
-		displayName = "[DDPM] Combat Uniform (Rolled-Up)";
+		displayName = "[DDPM] Fatigues (Rolled-Up)";
 		picture = "\PB_Main\ui\uniform_test.paa";
 		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
 		class ItemInfo: UniformItem
 		{
 			uniformModel = "-";
-			uniformClass = "PB_DDPM_ACU_B_R";
+			uniformClass = "PB_DDPM_Gora_B_RU";
 			containerClass = "Supply45";
 			mass = 50;
 			allowedSlots[] = {"701","801","901"};
 			armor = 0;
 		};
 	};
-	class PB_MTP_CTRG_U: Uniform_Base
+	class PB_DDPM_GoraG: Uniform_Base
 	{
 		scope = 2;
 		author = "Kartsa";
-		displayName = "[MTP] Combat Uniform";
+		displayName = "[DDPM] Fatigues with Gloves";
 		picture = "\PB_Main\ui\uniform_test.paa";
 		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
 		class ItemInfo: UniformItem
 		{
 			uniformModel = "-";
-			uniformClass = "PB_MTP_CTRG_U_B";
+			uniformClass = "PB_DDPM_GoraG_B";
 			containerClass = "Supply45";
 			mass = 50;
 			allowedSlots[] = {"701","801","901"};
 			armor = 0;
 		};
 	};
-	class PB_MTP_CTRG_U_R: Uniform_Base
+	class PB_DDPM_GoraG_RU: Uniform_Base
 	{
 		scope = 2;
 		author = "Kartsa";
-		displayName = "[MTP] Combat Uniform (Rolled-Up)";
+		displayName = "[DDPM] Fatigues with Gloves (Rolled-Up)";
 		picture = "\PB_Main\ui\uniform_test.paa";
 		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
 		class ItemInfo: UniformItem
 		{
 			uniformModel = "-";
-			uniformClass = "PB_MTP_CTRG_U_B_R";
+			uniformClass = "PB_DDPM_GoraG_B_RU";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_DPM_Gora: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[DPM] Fatigues";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_DPM_Gora_B";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_DPM_Gora_RU: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[DPM] Fatigues (Rolled-Up)";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_DPM_Gora_B_RU";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_DPM_GoraG: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[DPM] Fatigues with Gloves";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_DPM_GoraG_B";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_DPM_GoraG_RU: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[DPM] Fatigues with Gloves (Rolled-Up)";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_DPM_GoraG_B_RU";
 			containerClass = "Supply45";
 			mass = 50;
 			allowedSlots[] = {"701","801","901"};
@@ -236,6 +408,40 @@ class cfgWeapons
 			armor = 0;
 		};
 	};
+	class PB_MTP_ACU: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[MTP] Combat Uniform";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_MTP_ACU_B";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_MTP_ACU_R: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[MTP] Combat Uniform (Rolled-Up)";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_MTP_ACU_B_R";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
 	
 ////HELMETS///
 	class PB_DDPM_MK7: Helmet_Base
@@ -250,7 +456,79 @@ class cfgWeapons
         hiddenSelectionsTextures[] =
         {
          	"\PB_British_Kit\data\DDPM_MK7_CO.paa",
-         	"\PB_British_Kit\data\mk7_tan_01_co.paa",
+         	"\PB_British_Kit\data\Mk7_blk_01_co.paa",
+         	"\PB_M_Headgear\Mk7\tex\equip_d_co.paa"
+        };
+		class ItemInfo: HeadgearItem
+		{
+			mass = 30;
+			uniformModel = "\PB_M_Headgear\Mk7\H_Mk7_F.p3d";
+			hiddenSelections[] = { "Camo","Camo2","Camo3" };
+			modelSides[] =
+			{
+				TWest,
+				TCivilian
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 8;
+					passThrough = 0.5;
+				};
+			};
+		};
+	};
+	class PB_DPM_MK7: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "[DPM] Mk7 Helmet";
+		picture = "\PB_Main\ui\helmet_test.paa";
+		model = "\PB_M_Headgear\Mk7\H_Mk7_F.p3d";
+		hiddenSelections[] = { "Camo","Camo2","Camo3" };
+        hiddenSelectionsTextures[] =
+        {
+         	"\PB_British_Kit\data\DPM_MK7_CO.paa",
+         	"\PB_British_Kit\data\Mk7_blk_01_co.paa",
+         	"\PB_M_Headgear\Mk7\tex\equip_d_co.paa"
+        };
+		class ItemInfo: HeadgearItem
+		{
+			mass = 30;
+			uniformModel = "\PB_M_Headgear\Mk7\H_Mk7_F.p3d";
+			hiddenSelections[] = { "Camo","Camo2","Camo3" };
+			modelSides[] =
+			{
+				TWest,
+				TCivilian
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 8;
+					passThrough = 0.5;
+				};
+			};
+		};
+	};
+	class PB_MTP_MK7: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "[MTP] Mk7 Helmet";
+		picture = "\PB_Main\ui\helmet_test.paa";
+		model = "\PB_M_Headgear\Mk7\H_Mk7_F.p3d";
+		hiddenSelections[] = { "Camo","Camo2","Camo3" };
+        hiddenSelectionsTextures[] =
+        {
+         	"\PB_British_Kit\data\MTP_MK7_CO.paa",
+         	"\PB_British_Kit\data\Mk7_blk_01_co.paa",
          	"\PB_M_Headgear\Mk7\tex\equip_d_co.paa"
         };
 		class ItemInfo: HeadgearItem
@@ -276,7 +554,7 @@ class cfgWeapons
 	};
 	class PB_MTP_SF_H: Helmet_Base
 	{
-		author = "Jakx2210 (Caboose), Kartsa";
+		author = "Kartsa";
 		scope = 2;
 		weaponPoolAvailable = 1;
 		displayName = "[MTP] Helmet (SF)";
@@ -303,7 +581,7 @@ class cfgWeapons
 	};
 	class PB_MTP_H: Helmet_Base
 	{
-		author = "Jakx2210 (Caboose), Kartsa";
+		author = "Kartsa";
 		scope = 2;
 		weaponPoolAvailable = 1;
 		displayName = "[MTP] Helmet";
@@ -328,61 +606,46 @@ class cfgWeapons
 			};
 		};
 	};
-///VESTS///
-	class PB_DDPM_IOTV_01: Vest_Camo_Base
+	class PB_MTP_H_C: Helmet_Base
 	{
-		author = "Kartsa";
-		picture = "\PB_Main\ui\vest_test.paa";
+		author="Kartsa";
 		scope = 2;
-		displayName = "[DDPM] Carrier Rig (Rifleman)";
-		model = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_iotv_rifleman.p3d";
-		hiddenSelections[] = {"camo","camob","camo1","camo2"};
-		hiddenSelectionsTextures[] = 
+		weaponPoolAvailable = 1;
+		displayName="[MTP] Helmet (Camo)";
+		model="\A3\Characters_F\BLUFOR\headgear_b_helmet_camo";
+		picture = "\PB_Main\ui\helmet_test.paa";
+		hiddenSelections[]=
 		{
-			"PB_British_Kit\data\DDPM_IOTV_01_CO.paa",
-			"PB_British_Kit\data\ddpm_iotv1_co.paa",
-			"PB_British_Kit\data\ddpm_iotv2_co.paa",
-			"PB_British_Kit\data\ddpm_iotv3_co.paa"
+			"camo1",
+			"camo2"
 		};
-		class ItemInfo: ItemInfo
+		hiddenSelectionsTextures[]=
 		{
-			uniformModel = "rhsusf\addons\rhsusf_infantry\gear\vests\rhs_iotv_rifleman.p3d";
-			containerClass = "Supply140";
-			mass = 140;
+			"\PB_British_Kit\Data\mtp_helmet.paa",
+			"\A3\characters_f\common\data\ghillie2_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=40;
+			uniformModel="\A3\Characters_F\BLUFOR\headgear_b_helmet_camo";
+			modelSides[]={3,1};
+			hiddenSelections[]=
+			{
+				"camo1",
+				"camo2"
+			};
 			class HitpointsProtectionInfo
 			{
-				class Neck
+				class Head
 				{
-					HitpointName = "HitNeck";
-					armor = 12;
-					PassThrough = 0.4;
-				};
-				class Chest
-				{
-					HitpointName = "HitChest";
-					armor = 28;
-					PassThrough = 0.1;
-				};
-				class Diaphragm
-				{
-					HitpointName = "HitDiaphragm";
-					armor = "28 + 		3";
-					PassThrough = 0.1;
-				};
-				class Abdomen
-				{
-					hitpointName = "HitAbdomen";
-					armor = "28 + 		3";
-					passThrough = 0.1;
-				};
-				class Body
-				{
-					hitpointName = "HitBody";
-					passThrough = 0.1;
+					hitpointName="HitHead";
+					armor=6;
+					passThrough=0.5;
 				};
 			};
 		};
 	};
+///VESTS///
 	class PB_MTP_Modular_Lite: Vest_Camo_Base
 	{
 		author = "Kartsa";
@@ -468,39 +731,84 @@ class cfgWeapons
 			};
 		};
 	};
-	class PB_MTP_Modular_GL: Vest_Camo_Base
+	class PB_MTP_Carrier_Lite: Vest_Camo_Base
 	{
-		author = "Kartsa";
 		scope = 2;
-		displayName = "[MTP] Modular Carrier GL Rig";
+		author = "Kartsa";
 		picture = "\PB_Main\ui\vest_test.paa";
-		model = "\A3\Characters_F_Enoch\Vests\V_CarrierRigKBT_01_heavy_F.p3d";
+		Model = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+		displayName = "[MTP] Carrier Lite";
 		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {"PB_British_Kit\data\MTP_Modular.paa"};
-		descriptionShort = "Armor Level III";
-		class ItemInfo: ItemInfo
+		hiddenSelectionsTextures[] = {"PB_British_Kit\data\MTP_carrier_lite.paa"};
+		class ItemInfo: VestItem
 		{
-			uniformModel = "\A3\Characters_F_Enoch\Vests\V_CarrierRigKBT_01_heavy_F.p3d";
-			containerClass = "Supply130";
-			mass = 90;
+			uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+			hiddenSelections[] = {"camo"};
+			hiddenSelectionsTextures[] = {"PB_British_Kit\data\MTP_carrier_lite.paa"};
+			containerClass = "Supply120";
+			mass = 80;
 			class HitpointsProtectionInfo
 			{
 				class Chest
 				{
 					HitpointName = "HitChest";
-					armor = 35;
+					armor = 25;
 					PassThrough = 0.2;
 				};
 				class Diaphragm
 				{
 					HitpointName = "HitDiaphragm";
-					armor = 35;
+					armor = 25;
 					PassThrough = 0.2;
 				};
 				class Abdomen
 				{
 					hitpointName = "HitAbdomen";
-					armor = 35;
+					armor = 25;
+					passThrough = 0.2;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.2;
+				};
+			};
+		};
+	};
+	class PB_MTP_Carrier_Rig: Vest_Camo_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		picture = "\PB_Main\ui\vest_test.paa";
+		Model = "\A3\Characters_F\BLUFOR\equip_b_vest01";
+		displayName = "[MTP] Carrier Rig";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"PB_British_Kit\data\MTP_carrier_lite.paa"};
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest01";
+			hiddenSelections[] = {"camo"};
+			hiddenSelectionsTextures[] = {"PB_British_Kit\data\MTP_carrier_lite.paa"};
+			containerClass = "Supply130";
+			mass = 100;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
 					passThrough = 0.2;
 				};
 				class Body
