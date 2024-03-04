50aa3d58519c2fe12de6a50f113186f29390ac91
Kartsaa <60628805+Kartsaa@users.noreply.github.com>
2024-03-04 18:06:43 +0200

    Aussies have arrived
    - AMCU stuff
    - DDPM/DPM Uni scale

---
+++ PB_Australian_Kit/config.cpp
@@ -0,0 +1,560 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.10
+//https://mikero.bytex.digital/Downloads
+//'now' is Tue Dec 12 17:20:40 2023 : 'file' last modified on Mon Nov 09 00:32:17 2020
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class PB_Australian_Kit
+	{
+		version = "1.1.3";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = "1.1.3";
+		requiredAddons[] = 
+		{
+			"rhsgref_infantry",
+			"rhsusf_c_troops",
+			"rhsusf_infantry2",
+			"rhsusf_infantry3",
+			"PB_main",
+			"PB_M_Uniforms",
+			"PB_M_headgear",
+			"PB_M_Vests"
+		};
+	};
+};
+class CfgVehicles
+{
+////INHERITANCE////
+	class ContainerSupply;
+	class Supply31: ContainerSupply
+	{
+		maximumLoad = 31;
+	};
+	class Supply93: ContainerSupply
+	{
+		maximumLoad = 93;
+	};
+	class Supply45: ContainerSupply
+	{
+		maximumLoad = 45;
+	};
+	class B_Soldier_base_F;
+	class B_AssaultPack_Base;
+	class B_Kitbag_Base;
+///UNIS///
+	class PB_AMCU_ACU_B: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\A_CU\Aegis_OCombatUniform_F.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","camo2"};
+        hiddenSelectionsTextures[] = 
+        {
+            "PB_Australian_Kit\data\AMCU_acu_camo_co.paa",
+            "PB_Australian_Kit\data\AMCU_acu_camo1_co.paa",
+            "PB_Australian_Kit\data\TacGloves_khk_CO.paa"
+        };
+	};
+	class PB_AMCU_ACU_B_R: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\A_CU\Aegis_OCombatUniform_02_F.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","camo2"};
+        hiddenSelectionsTextures[] = 
+        {
+            "PB_Australian_Kit\data\AMCU_acu_camo_co.paa",
+            "PB_Australian_Kit\data\AMCU_acu_camo1_co.paa",
+            "PB_Australian_Kit\data\TacGloves_khk_CO.paa"
+        };
+	};
+///BACKPACKS////
+};
+class cfgWeapons
+{
+/////INHERITANCE////
+	class ContainerSupply;
+	class Supply120: ContainerSupply
+	{
+		maximumLoad = 120;
+	};
+	class Supply30: ContainerSupply
+	{
+		maximumLoad = 30;
+	};
+	class UniformItem;
+	class Uniform_Base;
+	class ItemCore;
+	class VestItem;
+	class Vest_Camo_Base: ItemCore
+	{
+		class ItemInfo;
+	};
+	class HeadgearItem;
+	class Helmet_Base: ItemCore
+	{
+		class ItemInfo;
+	};
+////UNIS////
+	class PB_AMCU_ACU: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[AMCU] Combat Uniform";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_AMCU_ACU_B";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_AMCU_ACU_R: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[AMCU] Combat Uniform (Rolled-Up)";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_AMCU_ACU_B_R";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+///HELMETS
+	class PB_AMCU_H: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "[AMCU] Helmet";
+		picture = "\PB_Main\ui\helmet_test.paa";
+		model = "\rhsusf\addons\rhsusf_infantry2\gear\head\opscore\opscore.p3d";
+		hiddenSelections[] = {"opscore","comtac","NSW","contour","UNCOV_FLAG","ms2000"};
+		allowedFacewear[] =
+		{
+			"rhs_googles_black", 1,  
+			"rhs_googles_clear", 1,
+			"rhs_googles_orange", 1,
+			"rhs_googles_yellow", 1,
+			"rhs_ess_black", 1,
+			"G_Bandanna_khk", 1,
+			"G_Bandanna_tan", 1,
+			"rhs_scarf", 1,
+			"rhsusf_shemagh_od", 1,
+			"rhsusf_shemagh2_od", 1,
+			"rhsusf_shemagh2_grn", 1,
+			"rhsusf_shemagh_tan", 1,
+			"rhsusf_shemagh2_tan", 1,
+			"rhsusf_shemagh2_gogg_grn", 1,
+			"rhsusf_shemagh_gogg_grn", 1,
+			"rhsusf_shemagh2_gogg_od", 1,
+			"rhsusf_shemagh_gogg_od", 1,
+			"rhsusf_shemagh_gogg_tan", 1,
+			"rhsusf_shemagh2_gogg_tan", 1,
+			"rhsusf_oakley_goggles_blk", 1,
+			"rhsusf_oakley_goggles_clr", 1,
+			"rhsusf_oakley_goggles_ylw", 1,
+		};
+		hiddenSelectionsTextures[] = 
+		{
+			"\PB_Australian_Kit\data\AMCU_opscore_co.paa",
+			"",
+			"",
+			"",
+			"",
+			""
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 30;
+			uniformModel = "\rhsusf\addons\rhsusf_infantry2\gear\head\opscore\opscore.p3d";
+			hiddenSelections[] = {"opscore","comtac","NSW","contour","UNCOV_FLAG","ms2000"};
+			modelSides[] =
+			{
+				TWest,
+				TCivilian
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 8;
+					passThrough = 0.5;
+				};
+			};
+		};
+	};
+	class PB_AMCU_H_H: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "[AMCU] Helmet (Headset)";
+		picture = "\PB_Main\ui\helmet_test.paa";
+		model = "\rhsusf\addons\rhsusf_infantry2\gear\head\opscore\opscore.p3d";
+		hiddenSelections[] = {"opscore","comtac","NSW","contour","UNCOV_FLAG","ms2000"};
+		allowedFacewear[] =
+		{
+			"rhs_googles_black", 1,  
+			"rhs_googles_clear", 1,
+			"rhs_googles_orange", 1,
+			"rhs_googles_yellow", 1,
+			"rhs_ess_black", 1,
+			"G_Bandanna_khk", 1,
+			"G_Bandanna_tan", 1,
+			"rhs_scarf", 1,
+			"rhsusf_shemagh_od", 1,
+			"rhsusf_shemagh2_od", 1,
+			"rhsusf_shemagh2_grn", 1,
+			"rhsusf_shemagh_tan", 1,
+			"rhsusf_shemagh2_tan", 1,
+			"rhsusf_shemagh2_gogg_grn", 1,
+			"rhsusf_shemagh_gogg_grn", 1,
+			"rhsusf_shemagh2_gogg_od", 1,
+			"rhsusf_shemagh_gogg_od", 1,
+			"rhsusf_shemagh_gogg_tan", 1,
+			"rhsusf_shemagh2_gogg_tan", 1,
+			"rhsusf_oakley_goggles_blk", 1,
+			"rhsusf_oakley_goggles_clr", 1,
+			"rhsusf_oakley_goggles_ylw", 1,
+		};
+		hiddenSelectionsTextures[] = 
+		{
+			"\PB_Australian_Kit\data\AMCU_opscore_co.paa",
+			"\rhsusf\addons\rhsusf_infantry2\gear\head\opscore\data\comtac_fg_co.paa",
+			"",
+			"",
+			"",
+			""
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 30;
+			uniformModel = "\rhsusf\addons\rhsusf_infantry2\gear\head\opscore\opscore.p3d";
+			hiddenSelections[] = {"opscore","comtac","NSW","contour","UNCOV_FLAG","ms2000"};
+			modelSides[] =
+			{
+				TWest,
+				TCivilian
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 8;
+					passThrough = 0.5;
+				};
+			};
+		};
+	};
+	class PB_AMCU_TCH: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "[AMCU] Combat Helmet";
+		picture = "\PB_Main\ui\helmet_test.paa";
+		model = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_F.p3d";
+		hiddenSelections[] = {"camo"};
+		allowedFacewear[] =
+		{
+			"rhs_googles_black", 1,  
+			"rhs_googles_clear", 1,
+			"rhs_googles_orange", 1,
+			"rhs_googles_yellow", 1,
+			"rhs_ess_black", 1,
+			"G_Bandanna_khk", 1,
+			"G_Bandanna_tan", 1,
+			"rhs_scarf", 1,
+			"rhsusf_shemagh_od", 1,
+			"rhsusf_shemagh2_od", 1,
+			"rhsusf_shemagh2_grn", 1,
+			"rhsusf_shemagh_tan", 1,
+			"rhsusf_shemagh2_tan", 1,
+			"rhsusf_shemagh2_gogg_grn", 1,
+			"rhsusf_shemagh_gogg_grn", 1,
+			"rhsusf_shemagh2_gogg_od", 1,
+			"rhsusf_shemagh_gogg_od", 1,
+			"rhsusf_shemagh_gogg_tan", 1,
+			"rhsusf_shemagh2_gogg_tan", 1,
+			"rhsusf_oakley_goggles_blk", 1,
+			"rhsusf_oakley_goggles_clr", 1,
+			"rhsusf_oakley_goggles_ylw", 1,
+		};
+		hiddenSelectionsTextures[] = {
+			"PB_Australian_Kit\data\AMCU_tch_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 30;
+			uniformModel = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_F.p3d";
+			hiddenSelections[] = {"camo"};
+			modelSides[] =
+			{
+				TWest,
+				TCivilian
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 8;
+					passThrough = 0.5;
+				};
+			};
+		};
+	};
+	class PB_AMCU_TCH_H: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		weaponPoolAvailable = 1;
+		displayName = "[AMCU] Combat Helmet (Headset)";
+		picture = "\PB_Main\ui\helmet_test.paa";
+		model = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_headset_F.p3d";
+		hiddenSelections[] = {"camo"};
+		allowedFacewear[] =
+		{
+			"rhs_googles_black", 1,  
+			"rhs_googles_clear", 1,
+			"rhs_googles_orange", 1,
+			"rhs_googles_yellow", 1,
+			"rhs_ess_black", 1,
+			"G_Bandanna_khk", 1,
+			"G_Bandanna_tan", 1,
+			"rhs_scarf", 1,
+			"rhsusf_shemagh_od", 1,
+			"rhsusf_shemagh2_od", 1,
+			"rhsusf_shemagh2_grn", 1,
+			"rhsusf_shemagh_tan", 1,
+			"rhsusf_shemagh2_tan", 1,
+			"rhsusf_shemagh2_gogg_grn", 1,
+			"rhsusf_shemagh_gogg_grn", 1,
+			"rhsusf_shemagh2_gogg_od", 1,
+			"rhsusf_shemagh_gogg_od", 1,
+			"rhsusf_shemagh_gogg_tan", 1,
+			"rhsusf_shemagh2_gogg_tan", 1,
+			"rhsusf_oakley_goggles_blk", 1,
+			"rhsusf_oakley_goggles_clr", 1,
+			"rhsusf_oakley_goggles_ylw", 1,
+		};
+		hiddenSelectionsTextures[] = {
+			"PB_Australian_Kit\data\AMCU_tch_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 35;
+			uniformModel = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_headset_F.p3d";
+			hiddenSelections[] = {"camo"};
+			modelSides[] =
+			{
+				TWest,
+				TCivilian
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 8;
+					passThrough = 0.5;
+				};
+			};
+		};
+	};
+///VESTS///
+	class PB_AMCU_Modular_Lite: Vest_Camo_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		displayName = "[AMCU] Modular Carrier Lite";
+		picture = "\PB_Main\ui\vest_test.paa";
+		model = "\A3\Characters_F_Enoch\Vests\V_CarrierRigKBT_01_light_F.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"PB_Australian_Kit\data\AMCU_Modular.paa"};
+		class ItemInfo: ItemInfo
+		{
+			uniformModel = "\A3\Characters_F_Enoch\Vests\V_CarrierRigKBT_01_light_F.p3d";
+			containerClass = "Supply130";
+			mass = 70;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.2;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.2;
+				};
+			};
+		};
+	};
+	class PB_AMCU_Modular: Vest_Camo_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		displayName = "[AMCU] Modular Carrier Vest";
+		picture = "\PB_Main\ui\vest_test.paa";
+		model = "\A3\Characters_F_Enoch\Vests\V_CarrierRigKBT_01_F.p3d";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"PB_Australian_Kit\data\AMCU_Modular.paa"};
+		descriptionShort = "Armor Level III";
+		class ItemInfo: ItemInfo
+		{
+			uniformModel = "\A3\Characters_F_Enoch\Vests\V_CarrierRigKBT_01_F.p3d";
+			containerClass = "Supply30";
+			mass = 50;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.2;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.2;
+				};
+			};
+		};
+	};
+	class PB_AMCU_Carrier_Lite: Vest_Camo_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		picture = "\PB_Main\ui\vest_test.paa";
+		Model = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+		displayName = "[AMCU] Carrier Lite";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"PB_Australian_Kit\data\AMCU_carrier_lite.paa"};
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+			hiddenSelections[] = {"camo"};
+			hiddenSelectionsTextures[] = {"PB_Australian_Kit\data\AMCU_carrier_lite.paa"};
+			containerClass = "Supply120";
+			mass = 80;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.2;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.2;
+				};
+			};
+		};
+	};
+	class PB_AMCU_Carrier_Rig: Vest_Camo_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		picture = "\PB_Main\ui\vest_test.paa";
+		Model = "\A3\Characters_F\BLUFOR\equip_b_vest01";
+		displayName = "[AMCU] Carrier Rig";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"PB_Australian_Kit\data\AMCU_carrier_lite.paa"};
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest01";
+			hiddenSelections[] = {"camo"};
+			hiddenSelectionsTextures[] = {"PB_Australian_Kit\data\AMCU_carrier_lite.paa"};
+			containerClass = "Supply130";
+			mass = 100;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.2;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.2;
+				};
+			};
+		};
+	};
+};
