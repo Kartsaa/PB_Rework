5f13bd4a7158da9fab9316a6ac987985e13a0a63
Kartsaa <60628805+Kartsaa@users.noreply.github.com>
2024-03-07 15:16:43 +0200

    canada

--- PB_Canadian_Kit/config.cpp
+++ PB_Canadian_Kit/config.cpp
@@ -48,6 +48,44 @@ class CfgVehicles
 	class B_AssaultPack_Base;
 	class B_Kitbag_Base;
 ///UNIS///
+
+	class PB_CADPATTW_Gora_B: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_01.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_Canadian_Kit\data\CADPATTW_Fatigues_co.paa"};
+	};
+	class PB_CADPATTW_Gora_B_RU: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_02.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_Canadian_Kit\data\CADPATTW_Fatigues_co.paa"};
+	};
+	class PB_CADPATTW_GoraG_B: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_03.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","insignia"};
+		
+		hiddenSelectionsTextures[] = {"\PB_Canadian_Kit\data\CADPATTW_Fatigues_co.paa","PB_British_Kit\data\TacGloves_Clk_CO.paa"};
+	};
+	class PB_CADPATTW_GoraG_B_RU: B_Soldier_base_F
+	{
+		scope = 1;
+		author = "Kartsa";
+		model = "\PB_M_Uniforms\Gora\U_Afghanka_04.p3d";
+		picture = "\PB_Main\UI\uniform_test.paa";
+		hiddenSelections[] = {"camo","camo1","insignia"};
+		hiddenSelectionsTextures[] = {"\PB_Canadian_Kit\data\CADPATTW_Fatigues_co.paa","PB_British_Kit\data\TacGloves_Clk_CO.paa"};
+	};
 ///BACKPACKS////
 };
 class cfgWeapons
@@ -75,6 +113,244 @@ class cfgWeapons
 	{
 		class ItemInfo;
 	};
+	class PB_CADPATTW_ACH_ESS: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		picture = "\PB_Main\ui\helmet_test.paa";
+		displayName = "[CADPAT-TW] CG634 Helmet (ESS)";
+		allowedFacewear[] = {"",6,"rhs_googles_black",3,"rhs_googles_clear",1};
+		model = "\rhsusf\addons\rhsusf_infantry\gear\head\ach_ess_01";
+		hiddenSelections[] = {"Camo","Camo1"};
+		hiddenSelectionsTextures[] = {"\PB_Canadian_Kit\data\cadpattw_ach_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 5;
+			uniformModel = "\rhsusf\addons\rhsusf_infantry\gear\head\ach_ess_01";
+			modelSides[] = {3,1};
+			hiddenSelections[] = {"camo"};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 0;
+					passThrough = 1;
+				};
+			};
+		};
+	};
+	class PB_CADPATTW_ACH: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		picture = "\PB_Main\ui\helmet_test.paa";
+		displayName = "[CADPAT-TW] CG634 Helmet";
+		allowedFacewear[] = {"",6,"rhs_googles_black",3,"rhs_googles_clear",1};
+		model = "\rhsusf\addons\rhsusf_infantry\gear\head\ach_camo";
+		hiddenSelections[] = {"Camo","Camo1"};
+		hiddenSelectionsTextures[] = {"\PB_Canadian_Kit\data\cadpattw_ach_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 5;
+			uniformModel = "\rhsusf\addons\rhsusf_infantry\gear\head\ach_camo";
+			modelSides[] = {3,1};
+			hiddenSelections[] = {"camo"};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 0;
+					passThrough = 1;
+				};
+			};
+		};
+	};
+	class PB_CADPATTW_ACH_HEADSET: Helmet_Base
+	{
+		author = "Kartsa";
+		scope = 2;
+		picture = "\PB_Main\ui\helmet_test.paa";
+		displayName = "[CADPAT-TW] CG634 Helmet (Headset)";
+		allowedFacewear[] = {"",6,"rhs_googles_black",3,"rhs_googles_clear",1};
+		model = "\rhsusf\addons\rhsusf_infantry\gear\head\ach_headset";
+		hiddenSelections[] = {"Camo","Camo1"};
+		hiddenSelectionsTextures[] = {"\PB_Canadian_Kit\data\cadpattw_ach_co.paa","\rhsusf\addons\rhsusf_infantry\gear\head\data\rhs_helmet_ach_acc_co.paa"};
+		class ItemInfo: HeadgearItem
+		{
+			mass = 5;
+			uniformModel = "\rhsusf\addons\rhsusf_infantry\gear\head\ach_headset";
+			modelSides[] = {3,1};
+			hiddenSelections[] = {"camo"};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName = "HitHead";
+					armor = 0;
+					passThrough = 1;
+				};
+			};
+		};
+	};
 ////UNIS////
 	
+	class PB_CADPATTW_Gora: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[CADPAT-TW] Fatigues";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_CADPATTW_Gora_B";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_CADPATTW_Gora_RU: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[CADPAT-TW] Fatigues (Rolled-Up)";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_CADPATTW_Gora_B_RU";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_CADPATTW_GoraG: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[CADPAT-TW] Fatigues with Gloves";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_CADPATTW_GoraG_B";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+	class PB_CADPATTW_GoraG_RU: Uniform_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		displayName = "[CADPAT-TW] Fatigues with Gloves (Rolled-Up)";
+		picture = "\PB_Main\ui\uniform_test.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_original_F";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "PB_CADPATTW_GoraG_B_RU";
+			containerClass = "Supply45";
+			mass = 50;
+			allowedSlots[] = {"701","801","901"};
+			armor = 0;
+		};
+	};
+////VESTS////
+class PB_CADPATTW_CARRIERLITE: Vest_Camo_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		picture = "\PB_Main\ui\vest_test.paa";
+		Model = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+		displayName = "[CADPAT-TW] Carrier Lite";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"PB_Canadian_Kit\data\CADPATTW_CARRIERLITE_co.paa"};
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest02";
+			hiddenSelections[] = {"camo"};
+			hiddenSelectionsTextures[] = {"PB_Canadian_Kit\data\CADPATTW_CARRIERLITE_co.paa"};
+			containerClass = "Supply120";
+			mass = 80;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.2;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.2;
+				};
+			};
+		};
+	};
+	class PB_CADPATTW_Carrier_Rig: Vest_Camo_Base
+	{
+		scope = 2;
+		author = "Kartsa";
+		picture = "\PB_Main\ui\vest_test.paa";
+		Model = "\A3\Characters_F\BLUFOR\equip_b_vest01";
+		displayName = "[CADPAT-TW] Carrier Rig";
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"PB_Canadian_Kit\data\CADPATTW_CARRIERLITE_co.paa"};
+		class ItemInfo: VestItem
+		{
+			uniformModel = "\A3\Characters_F\BLUFOR\equip_b_vest01";
+			hiddenSelections[] = {"camo"};
+			hiddenSelectionsTextures[] = {"PB_Canadian_Kit\data\CADPATTW_CARRIERLITE_co.paa"};
+			containerClass = "Supply130";
+			mass = 100;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName = "HitChest";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Diaphragm
+				{
+					HitpointName = "HitDiaphragm";
+					armor = 25;
+					PassThrough = 0.2;
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 25;
+					passThrough = 0.2;
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.2;
+				};
+			};
+		};
+	};
 };
