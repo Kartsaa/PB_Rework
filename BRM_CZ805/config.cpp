e71c0f1a1fda327b3ed641ef8f6aec8220e35451
Kartsaa <60628805+Kartsaa@users.noreply.github.com>
2024-03-15 17:47:07 +0200

    K2

---
+++ BRM_CZ805/config.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.10
+//https://mikero.bytex.digital/Downloads
+//'now' is Fri Mar 15 11:08:57 2024 : 'file' last modified on Thu Apr 13 20:08:37 2023
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class BRM_CZ805
+	{
+		author = "withered_maple";
+		name = "Broma Armory : CZ 805 series rifles";
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Weapons_F"};
+	};
+};
---
+++ BRM_K2/config.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.10
+//https://mikero.bytex.digital/Downloads
+//'now' is Fri Mar 15 10:57:30 2024 : 'file' last modified on Sun Jun 25 20:40:42 2023
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class BRM_K2
+	{
+		author = "withered_maple";
+		name = "Broma Armory : Daewoo K2 rifles";
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Weapons_F"};
+	};
+};
